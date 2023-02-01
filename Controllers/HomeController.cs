ac122c1e441842e25c4ba23a8d55364c91a0de3c
Daniel Ware <ware.daniel.a@gmail.com>
2023-01-31 21:21:21 -0600

    SSR dotnet with Vue

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vue3SSRdotnet.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        int number = DateTime.Now.Millisecond;
+        return View(number);
+    }
+}
